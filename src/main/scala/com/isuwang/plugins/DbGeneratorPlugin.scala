ff6568b6743f49af4e25c94833a6505261fdd9aa
jackliang <liangbinbin@iplas.com.cn>
2017-11-20 21:58:14 +0800

    新增基于数据库生成scala实体对象的插件

--- src/main/scala/com/isuwang/plugins/DbGeneratorPlugin.scala
+++ src/main/scala/com/isuwang/plugins/DbGeneratorPlugin.scala
@@ -27,9 +27,22 @@ object DbGeneratorPlugin extends AutoPlugin {
     generatedDbEntity := {
       val help =
         """
-      Please specific your ip, dbname, tableName for generated the entity..
+      Please specific your ip, dbname, package, tableName for generated the entity..
+      Default package is:
+        com.isuwang.soa.scala.dbName.entity
+      TableName is Optional, will generated all tableEntity if tableName not set
       For Example:
-        generatedDbEntity 127.0.0.1 crm crm_companies
+       1. generatedDbEntity 127.0.0.1 crm  com.isuwang.soa.scala.crm  crm_companies
+          will generate entity like:  com.isuwang.soa.scala.crm.entity.XXX
+          will generate enum like: com.isuwang.soa.scala.crm.enum.XXX
+
+       2. generatedDbEntity 127.0.0.1 crm
+          will generate entity like:  com.isuwang.soa.scala.crm.entity.XXX
+          will generate enum like: com.isuwang.soa.scala.crm.enum.XXX
+
+       3. generatedDbEntity 127.0.0.1 crm com.isuwang.soa.scala.crm.entity
+          will generate entity like: com.isuwang.soa.scala.crm.entity.entity.XXX
+          will generate enum like: com.isuwang.soa.scala.crm.entity.enum.XXX
 
       note: enum COMMENT FORMAT should be:
       Comment,EnumIndex:ChineseChars(EnglishChars);enumIndex:ChineseChars(EnglishChars);
@@ -44,42 +57,83 @@ object DbGeneratorPlugin extends AutoPlugin {
       } else {
         println(" start to generated db entity....args: ")
         args foreach println
-        val ipAddress = args(0)
-        val db = args(1)
-        val tableName = args(2)
-        val targetPath = (baseDirectory in Compile).value.getAbsolutePath
 
+        val (ipAddress,db,packageName,tableName) = args.size match {
+          case 0 =>
+            println(help)
+            ("","","","")
+          case 1 => println(help)
+            ("","","","")
+          case 2 => (args(0),args(1),s"com.isuwang.soa.scala.${args(1)}","")
+          case 3 => (args(0),args(1),args(2),"")
+          case 4 => (args(0), args(1),args(2), args(3))
+          case _ => println(help)
+            ("","","","")
+        }
+
+        val baseTargetPath = (baseDirectory in Compile).value.getAbsolutePath
         val connection = connectJdbc(ipAddress, db, user, passwd)
 
-        val columns = getTableColumnInfos(tableName.toLowerCase, db, connection)
-        println(s" oriTableName: ${tableName} -> tarTableName: ${toFirstUpperCamel(tableName)}")
-        println(s"columns: ${columns}")
-        toCaseClassEntity(toFirstUpperCamel(tableName), columns, targetPath)
+        if (!tableName.isEmpty) {
+          println(s" Found Specific tableName: ${tableName}, start to generateDbEntity..")
+          generateDbClass(tableName,db,connection,packageName,baseTargetPath)
+        } else {
+          println(s" No specific tableName found. will generate ${db} all tables..")
+
+          getTableNamesByDb(db,connection).foreach(item => {
+            println(s" start to generated ${db}.${item} entity file...")
+            generateDbClass(item,db,connection,packageName,baseTargetPath)
+          })
+        }
       }
     },
     logLevel in generatedDbEntity := Level.Debug
   )
 
 
-  def toCaseClassEntity(tableName: String, columns: List[(String, String, String)], targetPath: String) = {
+
+  def generateDbClass(tableName: String, db: String, connection: Connection, packageName: String, baseTargetPath: String): Unit = {
+    val columns = getTableColumnInfos(tableName.toLowerCase, db, connection)
+    val targetPath = baseTargetPath + "/src/main/scala/" + packageName.split("\\.").mkString("/") + "/"
+
+    toCaseClassEntity(toFirstUpperCamel(tableName),packageName, columns, targetPath)
+  }
+
+
+  def toCaseClassEntity(tableName: String, packageName: String,columns: List[(String, String, String)], targetPath: String) = {
     val sb = new StringBuilder(256)
-    sb.append(" package com.isuwang.soa.scala.entity \r\n")
+    sb.append(s" package ${packageName}.entity \r\n")
     sb.append("\r\n import java.sql.Timestamp \r\n")
     if (columns.exists(c => List("DATETIME", "DATE", "TIMESTAMP").contains(c._2))) {
       sb.append(" import java.sql.Timestamp \r\n")
     }
+
+    sb.append(" import wangzx.scala_commons.sql.ResultSetMapper \r\n\r\n ")
+
     sb.append(s" case class ${tableName} ( \r\n")
     columns.foreach(column => {
+      val hasValidEnum: Boolean = generateEnumFile(column._1, column._3, targetPath,packageName)
       sb.append(s" /** ${column._3} */ \r\n")
-      sb.append(toCamel(column._1)).append(": ").append(toScalaFieldType(column._2)).append(",\r\n")
-
-      generateEnumFile(column._1, column._3, targetPath)
+      sb.append(toCamel(column._1)).append(": ").append(
+        if (hasValidEnum) toFirstUpperCamel(column._1) else toScalaFieldType(column._2)
+      ).append(",\r\n")
     })
     sb.delete(sb.lastIndexOf(","), sb.lastIndexOf(",") + 1)
-    sb.append(")")
+    sb.append(") \t\n \t\n")
+
+    //添加数据库的隐式转换
+    /*
+    *  object TableName {
+    *     implicit val resultSetMapper: ResultSetMapper[TableName] = ResultSetMapper.meterial[TableName]
+    *  }
+    *
+    * */
+    sb.append(s" object ${tableName} { \r\n")
+    sb.append(s" \timplicit val resultSetMapper: ResultSetMapper[${tableName}] = ResultSetMapper.meterial[${tableName}] \r\n")
+    sb.append(" }")
 
 
-    val path = targetPath + "/src/main/scala/com/isuwang/soa/" + s"${tableName}.scala"
+    val path = targetPath + s"${toFirstUpperCamel(tableName)}.scala"
     generateEntityFile(sb.toString(), path)
 
   }
@@ -92,10 +146,54 @@ object DbGeneratorPlugin extends AutoPlugin {
     printWriter.close()
   }
 
-  def generateEnumFile(columnName: String, columnComment: String, targetPath: String) = {
+
+  def generateEnumFile(columnName: String, columnComment: String, targetPath: String,packageName: String) = {
+    /**
+      * EnumClass content:
+      *   class ${enumClassName} private(val id:Int, val name:String) extends DbEnum
+      *   object ${enumClassName} {
+      *      val NEW = new ${enumClassName}(0, "NEW")
+      *      def unknowne(id: Int) = new OrderStatus(id, s"<$id>")
+      *      def valueOf(id: Int): OrderStatus = id match {
+      *        case 0 => NEW
+      *        case _ => unknowne(id)
+      *      }
+      *      implicit object Accessor extends DbEnumJdbcValueAccessor[OrderStatus](valueOf)
+      *    }
+      *
+      * @param enums
+      * @return
+      */
+    def toEnumFileContent(enums: Array[(String, String)],packageName: String): String = {
+      val sb = new StringBuilder(256)
+      val enumClassName = toFirstUpperCamel(columnName)
+      sb.append(s" package ${packageName}.enum \r\n")
+      sb.append(" import wangzx.scala_commons.sql.DbEnum \r\n")
+      sb.append(" import wangzx.scala_commons.sql._ \r\n")
+
+      sb.append(s" class ${enumClassName} private(val id:Int, val name:String) extends DbEnum \r\n")
+      sb.append(s" object ${enumClassName} { \r\n")
+      enums.foreach(enum => {
+        sb.append(s"""\t val ${enum._2} = new ${enumClassName}(${enum._1},"${enum._2}") \r\n""")
+      })
+      sb.append(s"""\t def unknown(id: Int) = new ${enumClassName}(id, id+"") \r\n""")
+
+      sb.append(s"""\t def valueOf(id: Int): ${enumClassName} = id match { \r\n""")
+      enums.foreach(enum => {
+        sb.append(s" \t\t case ${enum._1} => ${enum._2} \r\n")
+      })
+      sb.append(" \t\t case _ => unknown(id) \r\n")
+      sb.append(" } \r\n")
+
+      sb.append(s" implicit object Accessor extends DbEnumJdbcValueAccessor[${enumClassName}](valueOf) \r\n")
+
+      sb.append("}")
+
+      sb.toString()
+    }
 
     columnComment match {
-      case enumRegx(a, b) => println(s" comment: ${a}, enumValue: ${b}")
+      case enumRegx(a, b) =>
         val enums: Array[(String, String)] = b.split(";").map(item => {
           item match {
             case singleEnumRegx(index, cnChars, enChars) =>
@@ -105,27 +203,15 @@ object DbGeneratorPlugin extends AutoPlugin {
           }
         })
 
-        val path = targetPath + "/src/main/scala/com/isuwang/soa/" + s"${toFirstUpperCamel(columnName)}.scala"
-        generateEntityFile(toEnumFileContent(enums), path)
-
-      case _ => println(s" Not match enum comment, skipped....${columnComment}")
+        val path = targetPath + s"${toFirstUpperCamel(columnName)}.scala"
+        generateEntityFile(toEnumFileContent(enums,packageName), path)
+        true
+      case _ =>
+        //println(s" Not match enum comment, skipped....${columnComment}")
+        false
     }
 
-    def toEnumFileContent(enums: Array[(String, String)]): String = {
-      val sb = new StringBuilder(256)
-      val enumClassName = toFirstUpperCamel(columnName)
-      sb.append(" package com.isuwang.soa.scala.enum \r\n")
-      sb.append(s" object ${enumClassName} extends Enumeration { \r\n")
-      sb.append(s" \t type ${enumClassName} = Value \r\n")
 
-      enums.foreach(enum => {
-        sb.append(s"""\t val ${enum._2} = Value(${enum._1},"${enum._2}") \r\n""")
-      })
-
-      sb.append("}")
-
-      sb.toString()
-    }
   }
 
   def toScalaFieldType(tableFieldType: String): String = {
@@ -150,6 +236,21 @@ object DbGeneratorPlugin extends AutoPlugin {
     }
 
     DriverManager.getConnection(url, user, passwd)
+  }
+
+
+  def getTableNamesByDb(db: String, connection: Connection)= {
+    val sql=s"select table_name from information_schema.tables where table_schema='${db}' and table_type='base table'";
+
+    val sqlStatement = connection.prepareStatement(sql)
+    val resultSet = sqlStatement.executeQuery()
+    val tableNames = mutable.MutableList[String]()
+    while (resultSet.next()) {
+      val tableName = resultSet.getString("table_name")
+      tableNames += tableName
+    }
+
+    tableNames
   }
 
 
