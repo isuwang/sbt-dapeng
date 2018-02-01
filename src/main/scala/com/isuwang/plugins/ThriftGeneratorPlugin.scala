e3cbde0c276bedad80c50b60b0e291c21867e1a2
jackliang <liangbinbin@iplas.com.cn>
2018-02-01 23:45:23 +0800

    enhance thrift generator plugin

--- src/main/scala/com/isuwang/plugins/ThriftGeneratorPlugin.scala
+++ src/main/scala/com/isuwang/plugins/ThriftGeneratorPlugin.scala
@@ -1,9 +1,12 @@
 package com.isuwang.plugins
 
+import java.io.File
+
 import com.github.dapeng.code.Scrooge
 import sbt.AutoPlugin
 import sbt.Keys._
 import sbt._
+
 import scala.io._
 
 // ApiPlugin: idlc
@@ -14,13 +17,10 @@ object ThriftGeneratorPlugin extends AutoPlugin{
   val generateFiles = taskKey[Seq[java.io.File]]("generate thrift file sources")
 
   def generateFilesTask = Def.task {
-    lazy val sourceFilesPath = (baseDirectory in Compile).value.getAbsolutePath + "/src/main/resources/thrifts"
-    lazy val targetFilePath =  (baseDirectory in Compile).value.getAbsolutePath + "/src/main"
-
+    lazy val sourceFilesPath = (baseDirectory in Compile).value.getAbsolutePath + "/src/main/resources"
+    lazy val targetFilePath =  (baseDirectory in Compile).value.getAbsolutePath + "/target/scala-2.12/src-generated"
 
     generateFiles(sourceFilesPath,targetFilePath)
-
-    Seq[java.io.File]()
   }
 
   override lazy val projectSettings = inConfig(Compile)(Seq(
@@ -30,25 +30,70 @@ object ThriftGeneratorPlugin extends AutoPlugin{
 
   def generateFiles(sourceFilePath: String, targetFilePath: String) = {
 
-    println("Welcome to use generate plugin")
-    val javaFileFolder = new File(targetFilePath + "/java")
-    if (!javaFileFolder.exists()) {
-      println(s" java file folder does no exists. create new one: ${javaFileFolder}")
-      javaFileFolder.mkdir()
-    }
+    println("Welcome to use generate plugin...")
+    val javaTargetPath = targetFilePath + "/java"
+    createForlder(javaTargetPath)
+    println(s" Java files target path: ${javaTargetPath}")
+
     Scrooge.main(Array("-gen", "java", "-all",
       "-in", sourceFilePath,
       "-out", targetFilePath))
 
-    val scalaFileFolder = new File(targetFilePath + "/scala")
-    if (!scalaFileFolder.exists()) {
-      println(s" java file folder does no exists. create new one: ${scalaFileFolder}")
-      scalaFileFolder.mkdir()
-    }
+    val javaFiles: Seq[File] = getFiles(javaTargetPath)
+    javaFiles.foreach(i => println(s" javaFile: ${i.getAbsolutePath}"))
+
+    val scalaTargetPath = targetFilePath + "/scala"
+    createForlder(scalaTargetPath)
+    println(s" scala files target path: ${scalaTargetPath}")
+
     Scrooge.main(Array("-gen", "scala", "-all",
       "-in", sourceFilePath,
       "-out", targetFilePath))
 
+    val scalaFiles: Seq[File] = getFiles(scalaTargetPath)
+    scalaFiles.foreach(i => println(s" scalaFiles: ${i.getAbsolutePath}"))
+
+    javaFiles ++ scalaFiles
+
+  }
+
+  def createForlder(path: String) = {
+    println(s" start to check whether the folder exists...${path}")
+    val paths = path.split("/")
+
+    var folder = "";
+
+    paths.foreach(p => {
+      folder += "/" + p
+      println(s" check folder: ${folder}")
+      val file = new File(folder)
+      if (!file.exists()) {
+        println(s" Folder Not found. create new one...${file.getAbsoluteFile}")
+        file.mkdir();
+      }
+    })
+  }
+
+
+  def main(args: Array[String]): Unit = {
+    val path = "/Users/jackliang/dev/github/sbt-dapeng" + "/target/scala-2.12/src-generated"
+
+    val file = new File(path);
+    println(s" folder exists: ${file.exists()}, isFolder: ${file.isDirectory}")
+
+    createForlder(path);
+
+    val file1 = new File(path);
+
+    println(s" folder exists: ${file1.exists()}, isFolder: ${file1.isDirectory}")
+  }
+
+  def getFiles(path: String): List[File] = {
+    if (!new File(path).isDirectory) {
+      List(new File(path))
+    } else {
+      new File(path).listFiles().flatMap(i => getFiles(i.getPath)).toList
+    }
   }
 
 }
