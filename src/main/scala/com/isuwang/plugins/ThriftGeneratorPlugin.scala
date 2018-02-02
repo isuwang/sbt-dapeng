81c0d6196807ef893c31a87cc51ac228332b4b26
jackliang <liangbinbin@iplas.com.cn>
2018-02-02 17:59:43 +0800

    bug fix for thrift generator

--- src/main/scala/com/isuwang/plugins/ThriftGeneratorPlugin.scala
+++ src/main/scala/com/isuwang/plugins/ThriftGeneratorPlugin.scala
@@ -16,7 +16,7 @@ object ThriftGeneratorPlugin extends AutoPlugin{
 
   val generateFiles = taskKey[Seq[java.io.File]]("generate thrift file sources")
 
-  val resourceGenerateFiles = taskKey[Seq[java.io.File]]("generate thrift file sources")
+  //val resourceGenerateFiles = taskKey[Seq[java.io.File]]("generate thrift file sources")
 
   def generateFilesTask = Def.task {
     lazy val sourceFilesPath = (baseDirectory in Compile).value.getAbsolutePath + "/src/main/resources"
@@ -25,25 +25,24 @@ object ThriftGeneratorPlugin extends AutoPlugin{
     generateFiles(sourceFilesPath,targetFilePath, resourceFilePath)
   }
 
-  def generateResourceFileTask = Def.task {
-    lazy val targetFilePath =  (baseDirectory in Compile).value.getAbsolutePath + "/target/scala-2.12/src_managed/main/resources"
-    val files: Seq[File] = getFiles(targetFilePath)
-    println("resource file size: " + files.size)
-    files.foreach(file => println(s" generated resource file: ${file.getAbsoluteFile}"))
-    files
-  }
+//  def generateResourceFileTask = Def.task {
+//    lazy val targetFilePath =  (baseDirectory in Compile).value.getAbsolutePath + "/target/scala-2.12/resource_managed/main"
+//    val files: Seq[File] = getFiles(targetFilePath)
+//    files.foreach(file => println(s" generated resource file: ${file.getAbsoluteFile}"))
+//    files
+//  }
 
   override lazy val projectSettings = inConfig(Compile)(Seq(
     generateFiles := generateFilesTask.value,
     sourceGenerators += generateFiles.taskValue
   ))
 
-  resourceGenerators in sbt.Keys.`package` += generateResourceFileTask.taskValue
+  //resourceGenerators in sbt.Keys.`package` += generateResourceFileTask.taskValue
 
   def generateFiles(sourceFilePath: String, targetFilePath: String, resourceFilePath: String) = {
 
-    println("Welcome to use generate plugin...")
-    val javaTargetPath = targetFilePath + "/java"
+    println("-------------------------Welcome to use generate plugin------------------")
+    val javaTargetPath = targetFilePath
     createForlder(javaTargetPath)
     println(s" Java files target path: ${javaTargetPath}")
 
@@ -54,7 +53,7 @@ object ThriftGeneratorPlugin extends AutoPlugin{
     val javaFiles: Seq[File] = getFiles(javaTargetPath)
     javaFiles.foreach(i => println(s" javaFile: ${i.getAbsolutePath}"))
 
-    val scalaTargetPath = targetFilePath + "/scala"
+    val scalaTargetPath = targetFilePath
     createForlder(scalaTargetPath)
     println(s" scala files target path: ${scalaTargetPath}")
 
@@ -74,11 +73,36 @@ object ThriftGeneratorPlugin extends AutoPlugin{
       IO.copy(Traversable((oldFile,newFile)))
     })
 
+    //code-generator默认会在dirRoot添加 /java/ 文件夹,需要删掉
+    val newJavaFiles: Seq[File] = javaFiles.map(javaFile => {
+      val newJavaFilePath = javaFile.getAbsolutePath.replaceFirst("java","");
+      val newJavaFile = new File(newJavaFilePath)
+      IO.copy(Traversable((javaFile,newJavaFile)))
+      newJavaFile
+    })
+
+    //code-generator默认会在dirRoot添加 /scala/ 文件夹,需要删掉
+    val newScalaFiles: Seq[File] = scalaFiles.map(scalaFile => {
+      val newScalaFilePath = scalaFile.getAbsolutePath.replaceFirst("/scala/","/");
+      val newScalaFile = new File(newScalaFilePath)
+      IO.copy(Traversable((scalaFile,newScalaFile)))
+      newScalaFile
+    })
+
     val newFiles = getFiles(newResourcePath)
     newFiles.foreach(f => println(s"new generatedFile: ${f.getAbsolutePath}"))
 
+    println(s"---------- start to remove old resource file from source folder: ${targetFilePath + "/resources"} -------")
     val oldFiles = new File(targetFilePath + "/resources")
-    if (oldFiles.isDirectory) oldFiles.delete()
+    deleteDir(oldFiles.getAbsolutePath)
+    println(s"---------- end to remove old resource file from source folder: ${targetFilePath + "/resources"} -------")
+
+
+    val oldJavaFiles = new File(targetFilePath + "/java")
+    deleteDir(oldJavaFiles.getAbsolutePath)
+
+    val oldScalaFiles = new File(targetFilePath + "/scala")
+    deleteDir(oldScalaFiles.getAbsolutePath)
 
     javaFiles ++ scalaFiles
 
@@ -103,20 +127,38 @@ object ThriftGeneratorPlugin extends AutoPlugin{
 
 
   def main(args: Array[String]): Unit = {
-    val path = "/Users/jackliang/dev/github/dapeng_test/dapeng_test-api/target/scala-2.12/src-generated/resources"
-    val files = getFiles(path);
+    //val path = "/Users/jackliang/dev/github/dapeng_test/dapeng_test-api/target/scala-2.12/src-generated/resources"
+    //val files = getFiles(path);
+    //println(s"file size: ${files.size}")
+    //files.foreach(f => println(f.getAbsoluteFile))
 
-    println(s"file size: ${files.size}")
+    val file = new File("/Users/jackliang/dev/github/dapeng_test/dapeng_test-api/target/scala-2.12/src_managed/main")
+//    deleteDir(file.getAbsolutePath)
 
-    files.foreach(f => println(f.getAbsoluteFile))
-//    val file = new File(path);
-//    println(s" folder exists: ${file.exists()}, isFolder: ${file.isDirectory}")
-//
-//    createForlder(path);
-//
-//    val file1 = new File(path);
-//
-//    println(s" folder exists: ${file1.exists()}, isFolder: ${file1.isDirectory}")
+    val javaFiles = getFiles(file.getAbsolutePath + "/java")
+    val newJavaFiles: Seq[File] = javaFiles.map(javaFile => {
+      val newJavaFilePath = javaFile.getAbsolutePath.replaceFirst("java","");
+      val newJavaFile = new File(newJavaFilePath)
+      IO.copy(Traversable((javaFile,newJavaFile)))
+      newJavaFile
+    })
+
+    //code-generator默认会在dirRoot添加 /scala/ 文件夹,需要删掉
+    val scalaFiles = getFiles(file.getAbsolutePath + "/scala")
+    val newScalaFiles: Seq[File] = scalaFiles.map(scalaFile => {
+      val newScalaFilePath = scalaFile.getAbsolutePath.replaceFirst("/scala/","/");
+      println(s" new scala file: ${newScalaFilePath}")
+      val newScalaFile = new File(newScalaFilePath)
+      IO.copy(Traversable((scalaFile,newScalaFile)))
+      newScalaFile
+    })
+
+      val oldJavaFiles = new File(file.getAbsolutePath + "/java")
+      deleteDir(oldJavaFiles.getAbsolutePath)
+
+      val oldScalaFiles = new File(file.getAbsolutePath + "/scala")
+      deleteDir(oldScalaFiles.getAbsolutePath)
+
   }
 
   def getFiles(path: String): List[File] = {
@@ -125,6 +167,18 @@ object ThriftGeneratorPlugin extends AutoPlugin{
     } else {
       new File(path).listFiles().flatMap(i => getFiles(i.getPath)).toList
     }
+  }
+
+  def deleteDir(dirPath: String):Unit = {
+    val file = new File(dirPath)
+    file.listFiles().foreach(f => {
+      if (f.isDirectory) {
+        deleteDir(f.getAbsolutePath)
+      } else {
+        f.delete()
+      }
+    })
+    if (file.exists()) file.delete()
   }
 
 }
